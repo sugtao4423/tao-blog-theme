a053d970f1116e052d877fbf866989863e5c4cc3
sugtao4423 <sugtao4423@gmail.com>
2023-05-12 04:39:25 +0900

    Migrate zip command to shell

---
+++ shell/zipper.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+ZIP_NAME='tao-blog-theme.zip'
+
+rm -f $ZIP_NAME
+zip -r $ZIP_NAME . \
+  -x '.*' \
+  -x 'node_modules/*' \
+  -x 'yarn.lock' \
+  -x 'shell/*' \
+  -x 'assets/sass/*' \
+  -x 'assets/css/built.min.css'
